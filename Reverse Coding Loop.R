9b6fa2949934d2e4255519a2858dfb82a30f4699
Meijun <meijuny21@gmail.com>
2023-10-13 21:12:15 +0200

    Reverse Coding with Felipe help!

---
+++ Reverse Coding Loop.R
@@ -0,0 +1,27 @@
+##Felipe
+ReverseCoding_scale10<-function(data,VarName){
+        output<-as.numeric()
+        for(i in seq_along(data[[VarName]])){
+                if(is.na(data[i,VarName])){
+                        output[i]<-NA
+                }else{
+                        output[i] <- (-1) * data[i, VarName] + 10
+                }
+        }
+        data<-cbind(data,output)
+        return(data)
+}
+
+##Meijun
+ReverseCoding_scale10<-function(data,VarName){
+        output<-as.numeric()
+        for(i in seq_along(data[[VarName]])){
+                ifelse(is.na(data[i,VarName]), output[i]<-NA, 
+                       output[i]<-(-1)*data[i,VarName]+10)
+        }
+        data<-cbind(data,output)
+        return(data)
+}
+
+##Example to use the function above
+ESS_immi<-ReverseCoding_scale10(data = ESS10_BE_immigrant,VarName = "imwbcnt")
